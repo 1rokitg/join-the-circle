5d35d73483023eedf25464c7ad2844689a261da1
rokitgg <pintosdsgn@gmail.com>
2026-08-24 20:56:37 +0200

    Add NotFound component for redirecting users to Whop season pass page

---
+++ src/app/whop/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Column, Heading, Text } from "@once-ui-system/core";
+import { useEffect, useState } from "react";
+
+export default function NotFound() {
+  const [secondsLeft, setSecondsLeft] = useState(5);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsLeft(secondsLeft - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [secondsLeft]);
+
+  if (secondsLeft <= 0) {
+    window.location.href = "https://whop.com/the-circle-vip/buy-season-pass/";
+  }
+
+  return (
+    <Column as="section" fill center paddingBottom="160">
+      <img
+        src="/images/whop.png"
+        alt="Whop Logo"
+        style={{ width: 26, height: 26, marginRight: 4 }}
+        className="rounded-full"
+      />
+      <Heading marginBottom="l" variant="display-default-xs">
+        Retirecting to Whop...
+      </Heading>
+      <Text onBackground="neutral-weak">{secondsLeft} seconds left</Text>
+    </Column>
+  );
+}
