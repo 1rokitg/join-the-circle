c294ec0a42a77e9881deab495fd119c03c591923
rokitgg <pintosdsgn@gmail.com>
2026-08-24 03:18:46 +0200

    Add Google Tag Manager script for enhanced tracking of content views
    - Declared a global interface for the dataLayer to facilitate Google Tag Manager integration.
    - Added Google Tag Manager script to the layout for improved tracking of user interactions and content views.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,12 @@ export async function generateMetadata() {
   });
 }
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -159,6 +165,7 @@ export default async function RootLayout({
               !function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");whop.setScope("biz_ROKYKZdV9YGZP7");whop.track("view_content");            `,
           }}
         />
+
         <meta name="og:title" content={home.title} />
         <meta name="og:description" content={home.description} />
         <meta name="og:image" content={ogImageUrl} />
