14a97130e14ec3f881e7dab91f32038be8b8b2a6
rokitgg <pintosdsgn@gmail.com>
2026-08-26 17:45:06 +0200

    Update blog post links and refactor layout for improved readability
    - Changed the blog post link to a static subscription URL.
    - Refactored the layout of the Post and Posts components for better readability and consistency, including adjustments to spacing and formatting.

--- src/components/blog/Post.tsx
+++ src/components/blog/Post.tsx
@@ -15,7 +15,7 @@ export default function Post({ post, thumbnail, direction }: PostProps) {
     <Card
       fillWidth
       key={post.slug}
-      href={`/blog/${post.slug}`}
+      href="https://rokitg.substack.com/subscribe?tag=propr"
       transition="micro-medium"
       direction={direction}
       border="transparent"
@@ -38,7 +38,13 @@ export default function Post({ post, thumbnail, direction }: PostProps) {
         />
       )}
       <Row fillWidth>
-        <Column maxWidth={28} paddingY="24" paddingX="l" gap="20" vertical="center">
+        <Column
+          maxWidth={28}
+          paddingY="24"
+          paddingX="l"
+          gap="20"
+          vertical="center"
+        >
           <Row gap="24" vertical="center">
             <Row vertical="center" gap="16">
               <Avatar src={person.avatar} size="s" />
