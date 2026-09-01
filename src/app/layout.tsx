278026b36868df4f3f5ad1c7b791455dd1ac1a98
rokitgg <pintosdsgn@gmail.com>
2026-09-01 04:32:57 +0200

    Add Tally form integration to layout
    - Introduced Tally configuration for a modal popup form with scroll-triggered opening.
    - Added script for Tally widget embedding to enhance user interaction on the page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -169,6 +169,29 @@ export default async function RootLayout({
               !function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");whop.setScope("biz_ROKYKZdV9YGZP7");whop.track("view_content");            `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.TallyConfig = {
+          "formId": "WO7ora",
+          "popup": {
+            "emoji": {
+              "text": "👋",
+              "animation": "wave"
+            },
+            "layout": "modal",
+            "open": {
+              "trigger": "scroll",
+              "scrollPercent": 10
+            },
+            "formEventsForwarding": true
+          }
+        };
+        `,
+          }}
+        />
+
+        <script async src="https://tally.so/widgets/embed.js"></script>
 
         <meta name="og:title" content={home.title} />
         <meta name="og:description" content={home.description} />
